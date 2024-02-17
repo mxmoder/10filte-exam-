182414abdd520619ef02c0a87e3ffbac459f6fdb
mxmoder <154695978+mxmoder@users.noreply.github.com>
2024-02-18 02:55:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -249,7 +249,9 @@ async def start(client, message):
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
             await message.reply_text(
-                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>" ,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=files_{fileid}')]]))
+                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>"
+            protect_content=True
+            )
             await verify_user(client, userid, token)
         else:
             return await message.reply_text(
